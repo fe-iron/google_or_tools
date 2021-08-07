a0fa91f7155d7859a8cd3502bcb05dd99fcb89ea
fe-iron <elahifaiz00@gmail.com>
2021-08-07 10:32:56 +0530

    delivery pick up via OR tools

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-"""Simple Vehicles Routing Problem."""
-
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
@@ -77,16 +75,25 @@ def create_data_model():
             536, 194, 798, 0
         ],
     ]
+    data['pickups_deliveries'] = [
+        [1, 6],
+        [2, 10],
+        [4, 3],
+        [5, 9],
+        [7, 8],
+        [15, 11],
+        [13, 12],
+        [16, 14],
+    ]
     data['num_vehicles'] = 4
-    data['starts'] = [1, 2, 15, 16]
-    data['ends'] = [0, 0, 0, 0]
+    data['depot'] = 0
     return data
 
 
 def print_solution(data, manager, routing, solution):
     """Prints solution on console."""
     print(f'Objective: {solution.ObjectiveValue()}')
-    max_route_distance = 0
+    total_distance = 0
     for vehicle_id in range(data['num_vehicles']):
         index = routing.Start(vehicle_id)
         plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
@@ -100,8 +107,8 @@ def print_solution(data, manager, routing, solution):
         plan_output += '{}\n'.format(manager.IndexToNode(index))
         plan_output += 'Distance of the route: {}m\n'.format(route_distance)
         print(plan_output)
-        max_route_distance = max(route_distance, max_route_distance)
-    print('Maximum of the route distances: {}m'.format(max_route_distance))
+        total_distance += route_distance
+    print('Total Distance of all routes: {}m'.format(total_distance))
 
 
 def main():
@@ -111,24 +118,21 @@ def main():
 
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                           data['num_vehicles'], data['starts'],
-                                           data['ends'])
+                                           data['num_vehicles'], data['depot'])
 
     # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
 
 
-    # Create and register a transit callback.
+    # Define cost of each arc.
     def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
+        """Returns the manhattan distance between the two nodes."""
         # Convert from routing variable Index to distance matrix NodeIndex.
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
         return data['distance_matrix'][from_node][to_node]
 
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-
-    # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
     # Add Distance constraint.
@@ -136,16 +140,28 @@ def main():
     routing.AddDimension(
         transit_callback_index,
         0,  # no slack
-        2000,  # vehicle maximum travel distance
+        3000,  # vehicle maximum travel distance
         True,  # start cumul to zero
         dimension_name)
     distance_dimension = routing.GetDimensionOrDie(dimension_name)
     distance_dimension.SetGlobalSpanCostCoefficient(100)
 
+    # Define Transportation Requests.
+    for request in data['pickups_deliveries']:
+        pickup_index = manager.NodeToIndex(request[0])
+        delivery_index = manager.NodeToIndex(request[1])
+        routing.AddPickupAndDelivery(pickup_index, delivery_index)
+        routing.solver().Add(
+            routing.VehicleVar(pickup_index) == routing.VehicleVar(
+                delivery_index))
+        routing.solver().Add(
+            distance_dimension.CumulVar(pickup_index) <=
+            distance_dimension.CumulVar(delivery_index))
+
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        routing_enums_pb2.FirstSolutionStrategy.PARALLEL_CHEAPEST_INSERTION)
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
